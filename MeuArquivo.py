0149d6cf02f3fc5e1d19fa53b8091c7a652b8c06
Paulo Romão <pauloromao.dev2020@gmail.com>
2022-09-15 23:35:41 -0300

    Definição da lógica de programação e README

---
+++ MeuArquivo.py
@@ -0,0 +1,11 @@
+#Importar base de dados
+
+#Visualizar base de dados
+
+#Faturamento por loja
+
+#Quantidade de produtos vendidos por loja
+
+#Ticket médio por produto em cada loja
+
+#Enviar email com relatorio
