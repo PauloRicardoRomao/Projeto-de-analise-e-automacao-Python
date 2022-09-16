624725f40ea23278ae35a659814527e26064c6bf
Paulo Romão <pauloromao.dev2020@gmail.com>
2022-09-16 00:33:20 -0300

    git commit -a -m "Importação, visuzalização da base de dados e filtragem das informações"

--- MeuArquivo.py
+++ MeuArquivo.py
@@ -1,8 +1,18 @@
+import pandas as pd
+import openpyxl
+
 #Importar base de dados
+tabela_vendas = pd.read_excel('vendas.xlsx')
+pd.set_option('display.max_columns', None)
+#pd.set_option('display.max_rows', None)
 
 #Visualizar base de dados
 
+#print(tabela_vendas[['ID Loja', 'Valor Final']])
+
 #Faturamento por loja
+Faturamento = tabela_vendas[['ID Loja', 'Valor Final']].groupby('ID Loja').sum()
+print(Faturamento)
 
 #Quantidade de produtos vendidos por loja
 
